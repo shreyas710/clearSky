d042bd44d9645a85900fae38d23d70a83f4af042
shreyas710 <shreyasnk5321@gmail.com>
2021-07-28 12:30:57 +0530

    https

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,21 @@
-const cityForm  = document.querySelector('form');
-const card = document.querySelector('.card');
-const details = document.querySelector('.details');
-const time = document.querySelector('img.time');
-const icon = document.querySelector('.icon img');
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 const forecast = new Forecast();
 
 // update ui
 const updateUI = (data) => {
-    // const cityDet = data.cityDet;
-    // const weather = data.weather;
+	// const cityDet = data.cityDet;
+	// const weather = data.weather;
 
-    // destructuring
-    const { cityDet, weather } = data;
+	// destructuring
+	const { cityDet, weather } = data;
 
-    // update details template
-    details.innerHTML = `
+	// update details template
+	details.innerHTML = `
         <h5 class="my-3">${cityDet.EnglishName}</h5>
         <div class="my-3">${weather.WeatherText}</div>
         <div class="display-4 my-4">
@@ -24,44 +24,45 @@ const updateUI = (data) => {
         </div>
     `;
 
-    // update night and day and icons
-    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
-    // if(weather.IsDayTime){
-    //     timeSrc = 'img/day.svg';
-    // }else{
-    //     timeSrc = 'img/night.svg';
-    // }
-    time.setAttribute('src', timeSrc);
+	// update night and day and icons
+	let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+	// if(weather.IsDayTime){
+	//     timeSrc = 'img/day.svg';
+	// }else{
+	//     timeSrc = 'img/night.svg';
+	// }
+	time.setAttribute("src", timeSrc);
 
-    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
-    icon.setAttribute('src', iconSrc)
+	const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+	icon.setAttribute("src", iconSrc);
 
-    // remove d-none class if present
-    if(card.classList.contains('d-none'))
-        card.classList.remove('d-none');
+	// remove d-none class if present
+	if (card.classList.contains("d-none")) card.classList.remove("d-none");
 };
 
-cityForm.addEventListener('submit', e => {
-    // prevent default action
-    e.preventDefault();
+cityForm.addEventListener("submit", (e) => {
+	// prevent default action
+	e.preventDefault();
 
-    // get city 
-    const city = cityForm.city.value.trim();
+	// get city
+	const city = cityForm.city.value.trim();
 
-    // reset city form
-    cityForm.reset();
+	// reset city form
+	cityForm.reset();
 
-    // update ui with new city
-    forecast.updateCity(city)
-        .then(data => updateUI(data))
-        .catch(err => console.log(err));
+	// update ui with new city
+	forecast
+		.updateCity(city)
+		.then((data) => updateUI(data))
+		.catch((err) => console.log(err));
 
-    // set local storage
-    localStorage.setItem('city', city);
+	// set local storage
+	localStorage.setItem("city", city);
 });
 
-if(localStorage.getItem('city')){
-    forecast.updateCity(localStorage.getItem('city'))
-        .then(data => updateUI(data))
-        .catch(err => console.log(err));
+if (localStorage.getItem("city")) {
+	forecast
+		.updateCity(localStorage.getItem("city"))
+		.then((data) => updateUI(data))
+		.catch((err) => console.log(err));
 }
